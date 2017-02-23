9b391f1c9d4b05966b578e7740dc869e754fd142
Andrew Garner <andrew@andrewgarner.com>
2017-02-23 11:09:01 +0000

    Present basic Corporate Information Pages to API

---
+++ app/presenters/publishing_api/corporate_information_page_presenter.rb
@@ -0,0 +1,25 @@
+module PublishingApi
+  class CorporateInformationPagePresenter
+    extend Forwardable
+    include UpdateTypeHelper
+
+    attr_reader :update_type
+
+    def initialize(corporate_information_page, update_type: nil)
+      self.corporate_information_page = corporate_information_page
+      self.update_type =
+        update_type || default_update_type(corporate_information_page)
+    end
+
+    def content
+      BaseItemPresenter
+        .new(corporate_information_page)
+        .base_attributes
+    end
+
+  private
+
+    attr_accessor :corporate_information_page
+    attr_writer :update_type
+  end
+end
